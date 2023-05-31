3b8b8e452c892b9b381bab8381f08e3594bf8252
Sharjeel Faiq <mrmalik610@gmail.com>
2023-05-31 23:49:27 +0500

    Tut2.ps1 created

---
+++ Tut2,.ps1
@@ -0,0 +1,21 @@
+# Local Scope
+function Test-Scope1 {
+    $localVariable = "I am a local variable."
+    # Write-Host $localVariable
+    function Test-Scope2 {
+        Write-Host $localVariable
+    }
+    Test-Scope2
+}
+
+Test-Scope1
+Write-Host $localVariable
+
+# Script Scope
+$scriptVariable = "I am a script variable";
+function Test-Scope {
+    Write-Host $scriptVariable
+}
+
+Test-Scope
+Write-Host $scriptVariable
