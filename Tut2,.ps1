1176c867b4b34c8e91c45eccef6a209aa2d4527f
Sharjeel Faiq <mrmalik610@gmail.com>
2023-06-02 00:05:46 +0500

    New Code

--- Tut2,.ps1
+++ Tut2,.ps1
@@ -1,6 +1,6 @@
 # Local Scope
 function Test-Scope1 {
-    $localVariable = "I am a local variable."
+    $localVariable = "I am a local variable.";
     # Write-Host $localVariable
     function Test-Scope2 {
         Write-Host $localVariable
@@ -19,3 +19,55 @@ function Test-Scope3 {
 
 Test-Scope3
 Write-Host $scriptVariable
+
+#Global Scope
+function Test-Scope4 {
+    $global:globalvariable = "I am a global variable.";
+    Write-Host $globalvariable;
+}
+
+Test-Scope4
+Write-Host $globalvariable
+
+# Private Scope
+
+function Test-Scope5 {
+    $private:privateVariable = "I am a private variable";
+    Write-Host $privateVariable
+    function Test-Scope6 {
+        Write-Host $privateVariable
+    }
+    Test-Scope6;
+}
+
+Test-Scope5
+Write-Host $privateVariable
+
+#Arrays
+$fruits = @("Apple", "Orange", "Banana");
+Write-Host $fruits[0]
+$fruits.Count
+$fruits.Length
+# $fruits.Sort();
+
+# Hashtable
+$person = @{
+    "Name" = "Jhon"
+    "Age" = 25
+}
+$person
+
+# Nested-Hashtable
+$process = @{
+    "Brand" = "Dell"
+    "Model" = "Optiplex"
+    "Specs" = @{
+        "RAM" = "4 GB"
+        "CPU" = "intel 15"
+        "CD/DVD Drive" = $false
+        "HDD" = "320 gb"
+    }
+}
+
+$process
+Get-Process
